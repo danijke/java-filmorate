e8f072ec5ac8703427c4f0ab3103933ce54d0387
danijke <dani.for.edu@yandex.kz>
2025-04-11 07:26:51 +0500

    init commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,27 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Review {
+    Long id;
+
+    @NotNull
+    String content;
+
+    @NotNull
+    boolean isPositive;
+
+    @NotNull
+    @Positive
+    Long userId;
+
+    @NotNull
+    @Positive
+    Long filmId;
+}
