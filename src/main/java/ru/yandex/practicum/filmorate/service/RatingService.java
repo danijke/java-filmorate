ad98dd8af50810fe034dad94a260b0dc414430b7
Saveliy <zagorzagorzagor2@yandex.ru>
2025-04-11 19:16:15 +0400

    Исправить коды ответа not found mpa genre

--- src/main/java/ru/yandex/practicum/filmorate/service/RatingService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/RatingService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Rating;
 import ru.yandex.practicum.filmorate.storage.RatingStorage;
 
@@ -27,7 +26,7 @@ public class RatingService {
 
     public void validateMpaId(Long id) {
         if (!ratingStorage.isMpaExits(id)) {
-            throw new ValidationException(
+            throw new NotFoundException(
                     String.format("нет в бд mpa id : %d", id)
             );
         }
