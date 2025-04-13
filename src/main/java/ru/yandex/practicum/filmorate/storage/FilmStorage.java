76adea93f909d545d172a3b510cbbb59e54e72d6
Stanislav Muzulev <muz-stanislav@yandex.ru>
2025-04-13 19:10:14 +0300

    style: добавлены пустые строки между методами интерфейса для checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -26,6 +26,8 @@ public interface FilmStorage {
     Collection<Film> getSortFilms(Long directorId, String sortBy);
 
     boolean containsFilmsByIds(Long... filmIds);
+
     Collection<Film> getFilmsLikedByUser(Long userId);
+
     int getLikesCount(Long filmId);
 }
